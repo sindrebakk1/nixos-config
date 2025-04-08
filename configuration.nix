46ea6211e0a16f7d7e4d053410d1c87acb7ee7dd
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-09 00:32:26 +0200

    move home manager config back to configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -127,6 +127,56 @@
     (nerdfonts.override { fonts = [ "RobotoMono" "DroidSansMono" ]; })
   ];
 
+  home-manager.users.sindreb = {
+    home.packages = with pkgs; [
+      lazygit
+      ghostty
+    ];
+    
+    programs.git = {
+      userName = "sindrebakk1";
+      userEmail = "sindre.bakken.naesset@gmail.com";
+    };
+    
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+    };
+
+    programs.waybar.enable = true;
+    wayland.windowManager.hyprland.enable = true;
+    wayland.windowManager.hyprland.settings = {
+      "$terminal" = "ghostty";
+      "$fileBrowser" = "nemo";
+      "$mod" = "SUPER";
+      exec-once = [
+	"$terminal"
+	"waybar & hyprpaper &"
+      ];
+      bind = [
+        "$mod, F, exec, firefox"
+	"$mod, E, exec, $fileBrowser"
+	"$mod, T, exec, $terminal"
+      ] ++ (
+        builtins.concatLists (builtins.genList (i:
+	  let ws = i + 1;
+	  in [
+	    "$mod, code:1${toString i}, workspace, ${toString ws}"
+	    "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+	  ]
+	)
+	9)
+      );
+    };
+
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
+
+    programs.nnn.enable = true;
+    
+    home.stateVersion = "24.11";
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- flake.nix
+++ flake.nix
@@ -13,12 +13,6 @@
       specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
-	home-manager.nixosModules.home-manager
-	{
-	  home-manager.useGlobalPkgs = true;
-	  home-manager.useUserPackages = true;
-	  home-manager.users.sindreb = ./home.nix;
-	}
       ];
     };
   };
