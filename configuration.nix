9c0267a6a79f51d7b580ea286b70a515be108a92
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-08 22:33:56 +0200

    update config

--- configuration.nix
+++ configuration.nix
@@ -146,18 +146,32 @@ in
     };
 
     programs.waybar.enable = true;
-    
-    wayland.windowManager.hyprland = {
-      enable = true;
-      settings = {
-        "$terminal" = "ghostty";
-  	"$fileBrowser" = "nemo";
-	exec-once = [
-	  "$terminal"
-	  "waybar & hyprpaper &"
-	];
-      };
+    wayland.windowManager.hyprland.enable = true;
+    wayland.windowManager.hyprland.settings = {
+      "$terminal" = "ghostty";
+      "$fileBrowser" = "nemo";
+      "$mod" = "SUPER";
+      exec-once = [
+	"$terminal"
+	"waybar & hyprpaper &"
+      ];
+      bind = [
+        "$mod, F, exec, firefox"
+	"$mod, E, exec, $fileBrowser"
+	"$mod, T, exec, $terminal"
+      ] ++ (
+        builtins.concatLists (builtins.genList (i:
+	  let ws = i + 1;
+	  in [
+	    "$mod, code:1${toString i}, workspace, ${toString ws}"
+	    "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+	  ]
+	)
+	9)
+      );
     };
+
+    home.sessionVariables.NIXOS_OZONE_WL = "1";
 
     programs.nnn.enable = true;
     
