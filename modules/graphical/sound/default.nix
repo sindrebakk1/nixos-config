39d38c5b6833618fd700c473c9d93feae889356f
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-10 00:28:18 +0200

    remove unused option

--- modules/graphical/sound/default.nix
+++ modules/graphical/sound/default.nix
@@ -1,12 +1,17 @@
-_: {
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    #jack.enable = true;
+{config, lib, ...}: {
+  options.settings.graphical.sound = {
+    enable = lib.mkEnableOption "Sound";
+  };
+  config = lib.mkIf config.settings.graphical.sound.enable {
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      audio.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      #jack.enable = true;
+    };
   };
 }
