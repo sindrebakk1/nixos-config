eb9c4fbfda426eec3b0f42b04389bb1ef2b2084f
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-10 17:34:46 +0200

    remove bad hyprland config items

--- modules/graphical/desktop/hyprland/default.nix
+++ modules/graphical/desktop/hyprland/default.nix
@@ -152,7 +152,6 @@
           dwindle = {
             pseudotile = true;
             preserve_split = true;
-            no_gaps_when_only = 0;
             smart_split = false;
             smart_resizing = false;
           };
