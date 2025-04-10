293dcac66698584a2fbeed62dc4f74c478688d01
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-11 00:34:17 +0200

    update language settings

--- modules/core/system/default.nix
+++ modules/core/system/default.nix
@@ -79,11 +79,12 @@
       defaultLocale = "en_US.UTF-8";
       extraLocaleSettings = {
         LC_ALL = "en_US.UTF-8";
-        LANGUAGE = "en_US:nb_NO";
+        LANGUAGE = "en_US";
         LC_TIME = "en_GB.UTF-8";
       };
       supportedLocales = [
         "en_US.UTF-8/UTF-8"
+        "en_GB.UTF-8/UTF-8"
       ];
     };
 
