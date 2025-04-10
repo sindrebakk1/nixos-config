000d4d8c986f215456af4e5d6c3b1bd4021d9b5b
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-10 23:57:18 +0200

    fix yazi module

--- modules/core/tui/yazi.nix
+++ modules/core/tui/yazi.nix
@@ -1,13 +1,15 @@
 _: {
-  home-manager.users.sindreb = {
-    progrmas.yazi = {
-      enable = true;
-      enableBashIntegration = true;
-      settings = {
-        manager = {
-	  show-hidden = true;
-	  sort-by = "modified";
-	};
+  config = {
+    home-manager.users.sindreb = {
+      progrmas.yazi = {
+        enable = true;
+	enableBashIntegration = true;
+	settings = {
+          manager = {
+	    show-hidden = true;
+	    sort-by = "modified";
+	  };
+        };
       };
     };
   };
