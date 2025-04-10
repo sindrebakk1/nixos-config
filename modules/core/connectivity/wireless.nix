4eed1f4bdbcf630cb78610c2f3a45b4b69e9e5f4
sindrebakk1 <sindre.bakken.naesset@gmail.com>
2025-04-10 15:31:11 +0200

    fix typo

--- modules/core/connectivity/wireless.nix
+++ modules/core/connectivity/wireless.nix
@@ -2,6 +2,6 @@
   options.settings.core.wireless.enable = lib.mkEnableOption "wireless";
 
   config = lib.mkIf config.settings.core.wireless.enable {
-    networking.networkManager.enable = true;
+    networking.networkmanager.enable = true;
   };
 }
